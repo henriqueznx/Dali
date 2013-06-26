0d53f32c90107187b551ac9b618c2a6240981dc7
Kévin <kevinchassagne@gmail.com>
2013-06-26 14:53:12 +0200

    slide down

--- public/js/open.js
+++ public/js/open.js
@@ -1,12 +1,6 @@
-function opendoors(){ // Pour faire l'effet de départ
+function opendoors(){ // Effet de départ
 	setTimeout(function(){document.getElementById('webdoc').className='transparent';},0);
 	setTimeout(function(){document.getElementById('pink').className='pink-open';},300);
 	setTimeout(function(){document.getElementById('grey').className='grey-open';},300);
  	setTimeout(function(){document.getElementById('webdoc').className='none';},800);
 }
-
-/*
-function openheader(){
-	setTimeout(function(){document.getElementById('head').className='open';},0);
-}
-*/
