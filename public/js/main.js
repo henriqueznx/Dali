27c65af092541097a7b8dde688011ed5302818a4
remydragyn <remy.dragyn@gmail.com>
2013-06-27 03:36:47 +0200

    Ajout du modèle de transition.
    Voir section#frame-one et section#frame-two dans le HTML
    Voir #frame-one et #frame-two dans le CSS (tout en bas)
    Voir JS (tout en Bas)

--- public/js/main.js
+++ public/js/main.js
@@ -1,76 +1,76 @@
 $(document).ready(function(){
-// LOADER ///////////////////
-	initHome();
-	$("#maincontent").hide();
+// // LOADER ///////////////////
+// 	initHome();
+// 	$("#maincontent").hide();
 
-	function initHome() {
-		var homeImgs = 
-		[
-		"public/uploads/apparition_visage_compotier_sur_plage.png",
-		"public/uploads/image_disappears.jpg",
-		"public/uploads/lion_cheval_dormeuse_invisibles.jpg",
-		"public/uploads/mae_west.jpg"
-		]
+// 	function initHome() {
+// 		var homeImgs = 
+// 		[
+// 		"public/uploads/apparition_visage_compotier_sur_plage.png",
+// 		"public/uploads/image_disappears.jpg",
+// 		"public/uploads/lion_cheval_dormeuse_invisibles.jpg",
+// 		"public/uploads/mae_west.jpg"
+// 		]
 	
-		var	count = 0, width = 0, imgsLenght = homeImgs.length, step;
-		step = 4 / imgsLenght;
+// 		var	count = 0, width = 0, imgsLenght = homeImgs.length, step;
+// 		step = 4 / imgsLenght;
 	
-		$.each(homeImgs, function(i, img){
-			var loader = new Image();
-			loader.src = img.toString();
-			$(loader).on("load", function(){
+// 		$.each(homeImgs, function(i, img){
+// 			var loader = new Image();
+// 			loader.src = img.toString();
+// 			$(loader).on("load", function(){
 	
-				width += step;
-				if(width == imgsLenght) {
-					$("#stache-left").css("cursor", "pointer");
-					$("#stache-right").css("cursor", "pointer");
-						// OVERTURE DES VOLETS
-							$(".stache-move").click(function(e) {
-								$("#maincontent").show();
-								TweenLite.set('.load-left, .load-right');
+// 				width += step;
+// 				if(width == imgsLenght) {
+// 					$("#stache-left").css("cursor", "pointer");
+// 					$("#stache-right").css("cursor", "pointer");
+// 						// OVERTURE DES VOLETS
+// 							$(".stache-move").click(function(e) {
+// 								$("#maincontent").show();
+// 								TweenLite.set('.load-left, .load-right');
 
-								TweenLite.to('.load-right', 1, 
-									{right: '-100%', ease: Power1.easeIn,
-									onComplete: function(e){$("#loader").remove()}
-									}
-								);
+// 								TweenLite.to('.load-right', 1, 
+// 									{right: '-100%', ease: Power1.easeIn,
+// 									onComplete: function(e){$("#loader").remove()}
+// 									}
+// 								);
 
-								TweenLite.to('.load-left', 1, 
-									{left: '-100%', ease: Power1.easeIn,
-									// onComplete: function(e){$(".load-left").remove()}
-									}
-								);
+// 								TweenLite.to('.load-left', 1, 
+// 									{left: '-100%', ease: Power1.easeIn,
+// 									// onComplete: function(e){$(".load-left").remove()}
+// 									}
+// 								);
 
-							});
-						// OVERTURE DES VOLETS FIN
-				}
-				$("#background_left").css("opacity", width);
-				$("#background_right").css("opacity", width);
-			});
-		});
-	}
+// 							});
+// 						// OVERTURE DES VOLETS FIN
+// 				}
+// 				$("#background_left").css("opacity", width);
+// 				$("#background_right").css("opacity", width);
+// 			});
+// 		});
+// 	}
 
-// SHOW TETE DE DALI
-	$("body").bind("keydown", function(e) {
-		var key_code = (e.keyCode ? e.keyCode : e.which);
-		if(key_code == 32){
-			$(".tete_dali").fadeIn();
-			$("#background_left").css("opacity", 0.5);
-			$("#background_right").css("opacity", 0.5);
-		}
-	});
-	$("body").bind("keyup", function(e) {
-		var key_code = (e.keyCode ? e.keyCode : e.which);
-		if(key_code == 32){
-			$(".tete_dali").fadeOut();
-			$("#background_left").css("opacity", 1);
-			$("#background_right").css("opacity", 1);
+// // SHOW TETE DE DALI
+// 	$("body").bind("keydown", function(e) {
+// 		var key_code = (e.keyCode ? e.keyCode : e.which);
+// 		if(key_code == 32){
+// 			$(".tete_dali").fadeIn();
+// 			$("#background_left").css("opacity", 0.5);
+// 			$("#background_right").css("opacity", 0.5);
+// 		}
+// 	});
+// 	$("body").bind("keyup", function(e) {
+// 		var key_code = (e.keyCode ? e.keyCode : e.which);
+// 		if(key_code == 32){
+// 			$(".tete_dali").fadeOut();
+// 			$("#background_left").css("opacity", 1);
+// 			$("#background_right").css("opacity", 1);
 
-		}
-	});
-// SHOW TETE DE DALI - FIN
+// 		}
+// 	});
+// // SHOW TETE DE DALI - FIN
 
-// LOADER - FIN ///////////////////
+// // LOADER - FIN ///////////////////
 
 	/* SLIDE  */
 	
@@ -115,5 +115,46 @@ $(document).ready(function(){
 	// 	$(this).fadeTo(100, 1);
 	// 	$('.fade').fadeTo(50, 0.5); 
 	// });
+
+
+// FRAME 1
+$('#frame-one').hide();
+	$('#frame1').click(function(e){
+		console.log("lol");
+		// $('#frame-one').show();
+		TweenLite.set('#home');
+		TweenMax.to('#home', 1, 
+			{'margin-left': '-100%', ease: Power1.easeIn,
+ 				onComplete: function(e){$("#home").hide(),
+ 							$('#frame-one').show();
+ 							TweenLite.fromTo('#frame-one', 1, 
+								{'margin-left': '130%', ease: Power4.easeInOut},
+								{'margin-left': '0%', ease: Power4.easeInOut}
+							)}
+ 			}
+ 		);
+
+	});
+// FRAME 1 - FIN
+
+// FRAME 1
+$('#frame-two').hide();
+	$('#frame2').click(function(e){
+		console.log("lol2");
+		// $('#frame-one').show();
+		TweenLite.set('#home');
+		TweenMax.to('#home', 1, 
+			{'margin-left': '-100%', ease: Power1.easeIn,
+ 				onComplete: function(e){$("#home").hide(),
+ 							$('#frame-two').show();
+ 							TweenLite.fromTo('#frame-two', 1, 
+								{'margin-left': '130%', ease: Power4.easeInOut},
+								{'margin-left': '0%', ease: Power4.easeInOut}
+							)}
+ 			}
+ 		);
+
+	});
+// FRAME 1 - FIN
 
 });
