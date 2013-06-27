1a61c444004a8936212ffb19f6b9e7ef5eb4ef2b
remydragyn <remy.dragyn@gmail.com>
2013-06-27 23:49:12 +0200

    Code réparer.

--- public/js/main.js
+++ public/js/main.js
@@ -11,78 +11,78 @@ document.getElementById('mute').addEventListener('click', function (e)
 }, false);
 /* FIN AUDIO */
 
-// // LOADER ///////////////////
-// 	initHome();
-// 	$("#maincontent").hide();
+// LOADER ///////////////////
+	initHome();
+	$("#maincontent").hide();
 
-// 	function initHome() {
-// 		var homeImgs = 
-// 		[
-// 		"public/uploads/apparition_visage_compotier_sur_plage.png",
-// 		"public/uploads/image_disappears.jpg",
-// 		"public/uploads/lion_cheval_dormeuse_invisibles.jpg",
-// 		"public/uploads/mae_west.jpg"
-// 		]
+	function initHome() {
+		var homeImgs = 
+		[
+		"public/uploads/apparition_visage_compotier_sur_plage.png",
+		"public/uploads/image_disappears.jpg",
+		"public/uploads/lion_cheval_dormeuse_invisibles.jpg",
+		"public/uploads/mae_west.jpg"
+		]
 	
-// 		var	count = 0, width = 0, imgsLenght = homeImgs.length, step;
-// 		step = 4 / imgsLenght;
+		var	count = 0, width = 0, imgsLenght = homeImgs.length, step;
+		step = 4 / imgsLenght;
 	
-// 		$.each(homeImgs, function(i, img){
-// 			var loader = new Image();
-// 			loader.src = img.toString();
-// 			$(loader).on("load", function(){
+		$.each(homeImgs, function(i, img){
+			var loader = new Image();
+			loader.src = img.toString();
+			$(loader).on("load", function(){
 	
-// 				width += step;
-// 				if(width == imgsLenght) {
-// 					$("#stache-left").css("cursor", "pointer");
-// 					$("#stache-right").css("cursor", "pointer");
-// 						// OVERTURE DES VOLETS
-// 							$(".stache-move").click(function(e) {
-// 								$("#maincontent").show();
-// 								TweenLite.set('.load-left, .load-right');
+				width += step;
+				if(width == imgsLenght) {
+					$("#stache-left").css("cursor", "pointer");
+					$("#stache-right").css("cursor", "pointer");
+						// OVERTURE DES VOLETS
+							$(".stache-move").click(function(e) {
+								$("#maincontent").show();
+								TweenLite.set('.load-left, .load-right');
 
-// 								TweenLite.to('.load-right', 1, 
-// 									{right: '-100%', ease: Power1.easeIn,
-// 									onComplete: function(e){$("#loader").remove()}
-// 									}
-// 								);
+								TweenLite.to('.load-right', 1, 
+									{right: '-100%', ease: Power1.easeIn,
+									onComplete: function(e){$("#loader").remove()}
+									}
+								);
 
-// 								TweenLite.to('.load-left', 1, 
-// 									{left: '-100%', ease: Power1.easeIn,
-// 									// onComplete: function(e){$(".load-left").remove()}
-// 									}
-// 								);
+								TweenLite.to('.load-left', 1, 
+									{left: '-100%', ease: Power1.easeIn,
+									// onComplete: function(e){$(".load-left").remove()}
+									}
+								);
 
-// 							});
-// 						// OVERTURE DES VOLETS FIN
-// 				}
-// 				$("#background_left").css("opacity", width);
-// 				$("#background_right").css("opacity", width);
-// 			});
-// 		});
-// 	}
+							});
+						// OVERTURE DES VOLETS FIN
+				}
+				$("#background_left").css("opacity", width);
+				$("#background_right").css("opacity", width);
+			});
+		});
+	}
 
-// // SHOW TETE DE DALI
-// 	$("body").bind("keydown", function(e) {
-// 		var key_code = (e.keyCode ? e.keyCode : e.which);
-// 		if(key_code == 32){
-// 			$(".tete_dali").fadeIn();
-// 			$("#background_left").css("opacity", 0.5);
-// 			$("#background_right").css("opacity", 0.5);
-// 		}
-// 	});
-// 	$("body").bind("keyup", function(e) {
-// 		var key_code = (e.keyCode ? e.keyCode : e.which);
-// 		if(key_code == 32){
-// 			$(".tete_dali").fadeOut();
-// 			$("#background_left").css("opacity", 1);
-// 			$("#background_right").css("opacity", 1);
+// SHOW TETE DE DALI
+	$("body").bind("keydown", function(e) {
+		var key_code = (e.keyCode ? e.keyCode : e.which);
+		if(key_code == 32){
+			$(".tete_dali").fadeIn();
+			$("#background_left").css("opacity", 0.5);
+			$("#background_right").css("opacity", 0.5);
+		}
+	});
+	$("body").bind("keyup", function(e) {
+		var key_code = (e.keyCode ? e.keyCode : e.which);
+		if(key_code == 32){
+			$(".tete_dali").fadeOut();
+			$("#background_left").css("opacity", 1);
+			$("#background_right").css("opacity", 1);
 
-// 		}
-// 	});
-// // SHOW TETE DE DALI - FIN
+		}
+	});
+// SHOW TETE DE DALI - FIN
 
-// // LOADER - FIN ///////////////////
+// LOADER - FIN ///////////////////
 
 	/* SLIDE  */
 	
